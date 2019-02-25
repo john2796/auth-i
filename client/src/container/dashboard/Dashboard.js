a0b28fce7ea74b1d57a0f63b7ff58f1722ee2039
john2796 <jbmiranda22796@gmail.com>
2019-02-25 13:43:37 -0800

    added registration compnent

---
+++ client/src/container/dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello this is the dashboard</h1>
+      </div>
+    );
+  }
+}
+
+export default dashboard;
